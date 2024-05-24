673499427a149a21d344ea951f46f80d85e49bc8
Gabriel <brgabrieltw@gmail.com>
2024-05-24 14:19:39 -0300

    feat: add initial project config files for dev environment

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { publicRoutes } from './public/public.routes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  ...publicRoutes
+];
